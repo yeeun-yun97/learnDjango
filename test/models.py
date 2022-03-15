f6fdbf9956ef4a1761463259aa05c185302ae13b
yyn9704@gmail.com <yyn9704@gmail.com>
2022-03-15 23:15:10 +0900

    create HelloApi

--- test/models.py
+++ test/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Calc(models.Model):
+	title = models.CharField(max_length=200)
+	body = models.TextField()
+	rate = models.FloatField()
+	count = models.IntegerField()
--- test/views.py
+++ test/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Calc
+from .serializers import CalcSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def HelloAPI(request):
+    return Response("Hello API!")
